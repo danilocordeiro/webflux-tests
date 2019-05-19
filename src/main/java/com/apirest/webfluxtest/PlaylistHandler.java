1dc3430a1304717c7da0f70ed58dc14446b31f73
Danilo Cordeiro <danilocordeiro.dev@gmail.com>
2019-05-19 09:49:16 -0300

    Add event stream

--- src/main/java/com/apirest/webfluxtest/PlaylistHandler.java
+++ src/main/java/com/apirest/webfluxtest/PlaylistHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-@Component
+//@Component
 public class PlaylistHandler {
 
     @Autowired
--- src/main/java/com/apirest/webfluxtest/controller/PlaylistController.java
+++ src/main/java/com/apirest/webfluxtest/controller/PlaylistController.java
@@ -3,11 +3,15 @@ package com.apirest.webfluxtest.controller;
 import com.apirest.webfluxtest.document.Playlist;
 import com.apirest.webfluxtest.services.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
-//@RestController
+import java.time.Duration;
+
+@RestController
 public class PlaylistController {
 
     @Autowired
@@ -26,6 +30,13 @@ public class PlaylistController {
     @PostMapping(value = "/playlist")
     public Mono<Playlist> save(@RequestBody Playlist playlist) {
         return playlistService.save(playlist);
+    }
+
+    @GetMapping(value = "/playlist/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tuple2<Long, Playlist>> getPlaylistByEvents() {
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(5));
+        Flux<Playlist> events = playlistService.findAll();
+        return Flux.zip(interval, events);
     }
 
 
